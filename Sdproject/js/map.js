83777f366c2c21b7c98b9a852d080d34095fadad
Dimitar Rangelov <dimitar.rangelov9@gmail.com>
2019-08-06 16:40:10 +0200

    SDproject Site

---
+++ Sdproject/js/map.js
@@ -0,0 +1,20 @@
+function initialize() {
+	var myOptions = {
+		zoom: 15,
+		center: new google.maps.LatLng(42.578076, 23.1680556), //change the coordinates
+		mapTypeId: google.maps.MapTypeId.ROADMAP,
+		scrollwheel: false,
+		mapTypeControl: false,
+		zoomControl: false,
+		streetViewControl: false
+	};
+	var map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
+	var marker = new google.maps.Marker({
+		map: map,
+		position: new google.maps.LatLng(42.578076, 23.1680556) //change the coordinates
+	});
+	google.maps.event.addListener(marker, "click", function() {
+		infowindow.open(map, marker);
+	});
+}
+google.maps.event.addDomListener(window, 'load', initialize);
